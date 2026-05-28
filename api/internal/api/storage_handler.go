7453dce3551ff83b0378c09efaec053b5196b789
MateusdiSousa <mateus69107@gmail.com>
2026-05-28 10:14:58 -0300

    Listagem de medias por usuario

--- api/internal/api/storage_handler.go
+++ api/internal/api/storage_handler.go
@@ -1,34 +1,76 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 	"log"
+	"time"
 
 	"github.com/MateusdiSousa/go-photos/api/domain/registro"
 	storagev1 "github.com/MateusdiSousa/go-photos/api/internal/proto"
+	"github.com/MateusdiSousa/go-photos/api/internal/service"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/google/uuid"
 	"github.com/minio/minio-go/v7"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type StorageHandler struct {
 	kafkaProducer *kafka.Producer
+	mediaService  *service.MediaService
 	storageClient *minio.Client
 	storagev1.UnimplementedStorageServiceServer
 }
 
 var (
 	REGISTRO_MEDIA = "registro.media"
+	EXPIRY_TIME    = time.Hour * 2
 )
 
-func NewStorageHandler(clientMinio *minio.Client, kafkaProducer *kafka.Producer) *StorageHandler {
-	return &StorageHandler{storageClient: clientMinio, kafkaProducer: kafkaProducer}
+func NewStorageHandler(clientMinio *minio.Client, kafkaProducer *kafka.Producer, mediaService *service.MediaService) *StorageHandler {
+	return &StorageHandler{
+		kafkaProducer: kafkaProducer,
+		storageClient: clientMinio,
+		mediaService:  mediaService}
 }
 
 var BUCKET_PHOTOS = "photos"
 
-func (service *StorageHandler) Upload(stream storagev1.StorageService_UploadServer) error {
+func RegistroMediaToMediaInfo(registros []*registro.RegistroMedia) []*storagev1.MediaInfo {
+	quantidadeRegistro := len(registros)
+	mediaInfo := make([]*storagev1.MediaInfo, quantidadeRegistro)
+	for index, registro := range registros {
+		mediaInfo[index] = &storagev1.MediaInfo{
+			Filename:  registro.Filename,
+			MediaType: registro.MediaType,
+			CreatedAt: registro.CreatedAt.String(),
+			Metadata:  "",
+			Url:       registro.FilePath,
+		}
+	}
+
+	return mediaInfo
+}
+
+func (server *StorageHandler) GetMedia(ctx context.Context, request *storagev1.GetMediaRequest) (*storagev1.GetMediaResponse, error) {
+	medias, err := server.mediaService.GetMediaPaged(ctx, request.UserId, int(request.PageSize), int(request.PageNum))
+	if err != nil {
+		log.Printf("Falha ao encontrar fotos do usuário: %s", err)
+		return nil, status.Error(codes.Internal, "Falha ao encontrar fotos do usuário.")
+	}
+	mediaInfo := RegistroMediaToMediaInfo(medias)
+	return &storagev1.GetMediaResponse{
+		MediaUrls: mediaInfo}, nil
+}
+
+func (server *StorageHandler) Upload(stream storagev1.StorageService_UploadServer) error {
+	if server.storageClient == nil {
+		log.Println("Erro crítico: Cliente S3/MinIO não foi inicializado!")
+		return status.Error(codes.Internal, "Falha ao conectar com o serviço de armazenamento")
+	}
+
 	pipeReader, pipeWriter := io.Pipe()
 
 	uuidFile := uuid.NewString()
@@ -39,7 +81,7 @@ func (service *StorageHandler) Upload(stream storagev1.StorageService_UploadServ
 
 	go func() {
 		defer pipeReader.Close()
-		_, err := service.storageClient.PutObject(
+		_, err := server.storageClient.PutObject(
 			stream.Context(),
 			BUCKET_PHOTOS,
 			uuidFile,
@@ -78,10 +120,12 @@ func (service *StorageHandler) Upload(stream storagev1.StorageService_UploadServ
 		}
 
 		n, err := pipeWriter.Write(req.Chunks)
+		if err != nil {
+			log.Printf("Falha ao escrever bytes no buffer: %s", err)
+		}
 
 		log.Printf("Quantidade de bytes escritos: %d bytes", n)
 		size += int64(n)
-
 	}
 
 	if err := <-errChan; err != nil {
@@ -96,7 +140,7 @@ func (service *StorageHandler) Upload(stream storagev1.StorageService_UploadServ
 		log.Printf("Falha ao gerar mensagem de evento: %s", err)
 	}
 
-	service.kafkaProducer.Produce(&kafka.Message{
+	server.kafkaProducer.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{Topic: &REGISTRO_MEDIA, Partition: kafka.PartitionAny},
 		Key:            []byte(uuidFile),
 		Value:          evento}, nil)
