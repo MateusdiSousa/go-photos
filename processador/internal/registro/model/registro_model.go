cc73ef0c0d9062c0f673cb32e5173e8d2484c962
MateusdiSousa <mateus69107@gmail.com>
2026-06-01 19:02:12 -0300

    Registro conseguindo recuperar arquivo temporario

--- processador/internal/registro/model/registro_model.go
+++ processador/internal/registro/model/registro_model.go
@@ -8,13 +8,24 @@ import (
 
 	registro_api "github.com/MateusdiSousa/go-photos/api/domain/registro"
 	model "github.com/MateusdiSousa/go-photos/processador/model"
+	storage "github.com/MateusdiSousa/go-photos/processador/s3"
 )
 
 func SetupRegistroModel() {
-	model.RegistrarExecuta("registro-upload", func(ctx context.Context, msg *sarama.ConsumerMessage, cmd *registro_api.RegistroComando) error {
-		log.Printf("COMANDO LIDO COM SUCESSO: %v", cmd)
-		return nil
-	})
+	model.RegistrarExecuta("registro-upload",
+		func(ctx context.Context, msg *sarama.ConsumerMessage, cmd *registro_api.RegistroComando) error {
+			bucket := cmd.Cadastro.Bucket
+			filedId := cmd.Cadastro.FileId
+
+			_, err := storage.GetTempMedia(ctx, bucket, filedId)
+			if err != nil {
+				return err
+			}
+
+			log.Println("Arquivo recuperado com sucesso!")
+
+			return nil
+		})
 
 	log.Println("Setup do Registro Model finalizado.")
 }
